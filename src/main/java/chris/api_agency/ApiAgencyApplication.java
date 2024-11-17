f9a1a1b8605edf2e1808f95e4486578817eeefe8
christianlsb <chrislsb09@gmail.com>
2024-11-17 10:55:38 -0300

    first commit

---
+++ src/main/java/chris/api_agency/ApiAgencyApplication.java
@@ -0,0 +1,13 @@
+package chris.api_agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgencyApplication.class, args);
+	}
+
+}
